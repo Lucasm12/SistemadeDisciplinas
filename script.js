1ae197497704e3b51fc80da14c5098a2a02506c2
LucasMarques <lucasm.desenvolvedor@gmail.com>
2024-02-27 11:04:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,9 @@ const disciplinas = {
 
   disciplina4: {
     "Aula 01(26-02-2024)":"https://drive.google.com/file/d/1EdE4BV3dcJCv84OGvQ79ZFQdYBVBPHoI/view?usp=drive_link",
-     "Divisão do Time":"https://docs.google.com/spreadsheets/d/1xD_jcVov_JMbo-sFJBb5RLJJ0QEMXzSnEijB6y_pXtE/edit?usp=sharing",
+     // "Aula 01(26-02-2024) Divisão do Time":"https://docs.google.com/spreadsheets/d/1xD_jcVov_JMbo-sFJBb5RLJJ0QEMXzSnEijB6y_pXtE/edit?usp=sharing",
+    <a href="https://docs.google.com/spreadsheets/d/1xD_jcVov_JMbo-sFJBb5RLJJ0QEMXzSnEijB6y_pXtE/edit?usp=sharing" target="_blank">Aula 01(26-02-2024) Divisão do Time</a>
+
   },
 };
 
