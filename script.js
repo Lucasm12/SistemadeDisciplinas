403f82d6d25b0d4a9e8a1ea38a83e03a052a1288
LucasMarques <lucasm.desenvolvedor@gmail.com>
2024-03-18 13:20:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ const disciplinas = {
     "Aula 02(05-03-2024) Documento de Especificações":"https://drive.google.com/file/d/1M6xkijldIGfHkw1tUMIKbkteBlEZbvMc/view?usp=drive_link",
     "Aula 02(05-03-2024) Requisito das Telas":"https://drive.google.com/file/d/1f_1t9eJb00JWm1afFrDBCVP63Co29yai/view?usp=drive_link",
     "Aula 03(19-03-2024)": "https://drive.google.com/file/d/1RVwWlrZc-kUCwsZjNOZQnM8H1du6iFN3/view?usp=drive_link",
-    // "Aula 03(19-03-2024)": "https://drive.google.com/file/d/1RVwWlrZc-kUCwsZjNOZQnM8H1du6iFN3/view?usp=drive_link",
+    "Revisão(19-03-2024)": "https://drive.google.com/file/d/1rTPvyXaKGP1CsKKJwkLVLU-qddXSnW3r/view?usp=drive_link",
     // "Respostas 03(19-03-2024)": "https://drive.google.com/file/d/1lfud76Y7NrwYST1lFywZ1fHaLaeXeFLt/view?usp=drive_link",
     
     
