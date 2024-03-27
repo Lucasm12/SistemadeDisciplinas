0012e05a08f1bf2b63c45cb51e575c33a8b1ba8b
LucasMarques <lucasm.desenvolvedor@gmail.com>
2024-03-26 21:25:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ const disciplinas = {
     "Aula 04(19-03-2024)": "https://drive.google.com/file/d/1RVwWlrZc-kUCwsZjNOZQnM8H1du6iFN3/view?usp=drive_link",
     "Respostas 03(19-03-2024)": "https://drive.google.com/file/d/1fc-53Km2bqmRxoEp7iI-fb5NZA7uToFS/view?usp=drive_link",
     "Revisão(19-03-2024)": "https://drive.google.com/file/d/1rTPvyXaKGP1CsKKJwkLVLU-qddXSnW3r/view?usp=drive_link",
+    "1º Avaliação(26-03-2024)": "https://drive.google.com/file/d/1YnNGBosczvG2lIoCBGmoQT8Dc52amY4y/view?usp=drive_link",
      
     
     
